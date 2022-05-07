8b428ce980b66634407d17870a9614b85fb63705
muhamedh <muhammed.hamzic@gmail.com>
2022-05-07 17:49:06 +0200

    t1/t2/t3 completed

--- js/router.js
+++ js/router.js
@@ -1,3 +1,124 @@
+$("#options").hide();
+$("#question").hide();
+$("#score").hide();
+
 var myp5 = new p5(welcomeSketch, 'view_container');
+var taskCounter = 0;
+var score = 0;
+
+$('body').one( "keypress", function() {
+   myp5.remove();
+   myp5 = new p5(taskOne, 'view_container');
+   $("#options").show();
+   $("#question").show();
+   $("#score").show();
+   taskCounter = taskCounter + 1;
+   
+   t1();
+   
+});
+
+var setQuestion = function(taskText, option1, option2,option3,option4){
+    $("#task-description").html(taskText);
+    $('#option1').text(option1);
+    $('#option2').text(option2);
+    $('#option3').text(option3);
+    $('#option4').text(option4);
+}
+var disableEventHandler = function(){
+    $("#option1").off();
+    $("#option2").off();
+    $("#option3").off();
+    $("#option4").off();
+}
+var t4 = function(){
+    console.log('hello from t4');
+}
+var t3 = function(){
+    console.log("hello from t3");
+    myp5.remove();
+    myp5 = new p5(taskThree, 'view_container');
+    setQuestion("a box is blue, a circle is red, if we want a yellow triangle we type?", "yellow(triange)", "triangle(yellow)", "star(yellow)", "box(red)");
+    $('#option1').one("click", function(){
+        disableEventHandler();
+        t4();
+    });
+    $('#option2').one("click", function(){
+        console.log('correct!');
+        score += 100;
+        $("#score-holder").html("SCORE =" + score);
+        disableEventHandler();
+        t4();
+    });
+    $('#option3').one("click", function(){
+        console.log('incorrect!');
+        
+        disableEventHandler();
+        t4();
+    });
+    $('#option4').one("click", function(){
+        console.log('incorrect!');
+        disableEventHandler();
+        t4();
+    });
+}
+var t2 = function(){
+    myp5.remove();
+    myp5 = new p5(taskTwo, 'view_container');
+    console.log('hi from t2'); 
+    setQuestion("one object draws over the first, how did we end up with this picture?", "blue(box), red(box)", "box(blue), box(yellow)", "box(blue), box(red)", "circle(red), blue(box)");
+    $('#option1').one("click", function(){
+        console.log('incorrect!');
+        disableEventHandler();
+        t3();
+    });
+    $('#option2').one("click", function(){
+        console.log('incorrect!');
+        disableEventHandler();
+        t3();
+    });
+    $('#option3').one("click", function(){
+        console.log('correct!');
+        score += 100;
+        $("#score-holder").html("SCORE =" + score);
+        disableEventHandler();
+        t3();
+    });
+    $('#option4').one("click", function(){
+        console.log('incorrect!');
+        disableEventHandler();
+        t3();
+    });
+}
+
+var t1 = function(){
+    taskCounter++;
+
+    setQuestion("circle(red) displays a red circle,<br> select the option which will display a blue circle!","circle(blue)","blue(circle)","circle(bleu)", "circle(red)");
+    $('#option1').one("click", function(){
+        console.log('correct!');
+        score += 100;
+        $("#score-holder").html("SCORE =" + score);
+        disableEventHandler();
+        t2();
+    });
+    $('#option2').one("click", function(){
+        console.log('incorrect!');
+        disableEventHandler();
+        t2();
+    });
+    $('#option3').one("click", function(){
+        console.log('incorrect');
+        disableEventHandler();
+        t2();
+    });
+    $('#option4').one("click", function(){
+        console.log('incorrect!');
+        disableEventHandler();
+        t2();
+    });
+}
+
+
 
 
--- js/sketch.js
+++ js/sketch.js
@@ -1,15 +1,17 @@
-// save this file as sketch.js
-// Sketch One
+
+
 var welcomeSketch = function(p){
+
+    var width = document.getElementById("view_container").offsetWidth;
+
     p.preload = function(){
         inconsolata = p.loadFont('assets/inconsolata.ttf');
     }
     p.setup = function(){
-        p.createCanvas(document.getElementById("view_container").offsetWidth, 400, p.WEBGL);
+        p.createCanvas(width, 400,p.WEBGL);
         p.textFont(inconsolata);
-        p.textAlign(p.CENTER, p.CENTER);
+        p.textAlign(p.CENTER);
         p.textSize(400 / 10);
-        p.string = 'Welcome! :) To The Hour of Code';
         p.x = 0;
         p.fade = 0;
     }
@@ -20,7 +22,7 @@ var welcomeSketch = function(p){
         }else if(p.x == 1){
             p.printFadeMessage('You will learn programming function basics');
         }else if(p.x == 2){
-            p.printFadeMessage('Press X to start!')
+            p.printFadeMessage('Press [any key] to start!')
         }else{
             p.x = 0;
         }
@@ -29,47 +31,52 @@ var welcomeSketch = function(p){
     p.printFadeMessage = function(text){
         p.fill(30,182,14, p.fade);
         p.text(text,0,0);
-        if(p.fade >= 255){
+        if(p.fade >= 255){ 
             p.fade = 0;
             p.x++;
         }else{
             p.fade += 1;
         }
     }
-}
+};
+var taskThree = function(p){
+    var width = document.getElementById("view_container").offsetWidth;
 
-var s = function (p) { // p could be any variable name
-    var x = 100;
-    var y = 100;
     p.setup = function () {
-        p.createCanvas(400, 200);
+        p.createCanvas(width, 400);
     };
 
     p.draw = function () {
         p.background(0);
-        p.fill(255);
-        p.rect(x, y, 50, 50);
+        p.fill(243,234,51);
+        p.triangle(width/2, 80, width/2 - 70, 250, width/2 + 70, 250);
     };
-};
+}
+var taskTwo = function(p){
+    var width = document.getElementById("view_container").offsetWidth;
 
-
-// Sketch Two
-var t = function (p) {
-    var x = 100.0;
-    var y = 100;
-    var speed = 2.5;
     p.setup = function () {
-        p.createCanvas(400, 200);
+        p.createCanvas(width, 400);
     };
 
     p.draw = function () {
-        p.background(100);
-        p.fill(1);
-        x += speed;
-        if (x > p.width) {
-            x = 0;
-        }
-        p.ellipse(x, y, 50, 50);
-
+        p.background(0);
+        p.fill(14,39,182);
+        p.rect(width/2 - 50, 150, 100);
+        p.fill(228,2,0);
+        p.rect(width/2 - 50 + 30, 180, 100);
     };
-};
+}
+var taskOne = function(p){
+    var width = document.getElementById("view_container").offsetWidth;
+
+    p.setup = function () {
+        p.createCanvas(width, 400);
+    };
+
+    p.draw = function () {
+        p.background(0);
+        p.fill(14,39,182);
+        p.circle(width/2 - 50, 150, 100);
+    };
+}
